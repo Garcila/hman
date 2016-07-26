3ead6caa586a1524f01bcadedf96c7f06689c180
German Arcila <germanarcila@gmail.com>
2016-07-26 11:47:27 -0400

    ai working

--- hman.js
+++ hman.js
@@ -1,15 +1,59 @@
-$(document).ready(function(){
+var word;
+var error_count = 0;
+var used_letters = [];
+var index_chosen_letter= [];
+var won = 0;
+var lost = 0;
+var word_computer_generated;
+var game_type = 0;
 
+function rand() {
+  return Math.floor(Math.random()*(7-3+1)+3);
+}
+//function to get random word from API.  AI game
+function RandomWord() {
+  var random_word_lenght = rand();
+  var requestStr = "http://randomword.setgetgo.com/get.php?len=" + random_word_lenght;
+
+  $.ajax({
+    type: "GET",
+    url: requestStr,
+    dataType: "jsonp",
+    jsonpCallback: 'RandomWordComplete'
+  });
+}
+
+function RandomWordComplete(data) {
+  word = data.Word.toUpperCase();
+  console.log(data.Word);
+  console.log(word);
+  separateWord(word);
+  getLetter();
+}
+
+aiOrHuman();
+
+$(document).ready(function(){
+  if(game_type === 0) {
+    $('.word_to_guess').hide();
+  }
+})
+
+  function aiOrHuman() {
+    if(game_type === 0) {
+      RandomWord();
+    } else {
+      $('.word_to_guess').show();
+      getWord();
+    }
+  }
+// })
+
+  //reveals or hides side menu
   $('.menu-toggle').bind('click', function() {
       $('body').toggleClass('menu-open')
       return false;
   });
-
-  var error_count = 0;
-  var used_letters = [];
-  var index_chosen_letter= [];
-  var won = 0;
-  var lost = 0;
 
   function getWord() {
     $('.word_to_guess').off().on('keypress', function(e) {
@@ -24,35 +68,36 @@ $(document).ready(function(){
       }
     });
   }
-  getWord();
+// })
 
-  function separateWord(word) {
-    word_array = [];
-    word_array = word.split('');
-    // console.log('the word array is ' + word_array);
-    var letter_count = word_array.length;
-    lines = word_array.map(function(item) { return item, '_ '});
-    $('.lines').empty();
-    $('.lines').append(lines);
-    console.log(lines.join(''));
+function separateWord(word) {
+  word_array = [];
+  word_array = word.split('');
+  console.log(word_array);
+  console.log('the word array is ' + word_array);
+  var letter_count = word_array.length;
+  lines = word_array.map(function(item) { return item, '_ '});
+  $('.lines').empty();
+  $('.lines').append(lines);
+  console.log(lines.join(''));
+}
+
+function getLetter() {
+  $('.Grid-cell').off().on('click', function(e) {
+    var chosen_letter = $(this).text();
+    console.log('you choose: ' + chosen_letter)
+  if(used_letters.indexOf(chosen_letter) > -1) {
+    // alert('you already played that letter');
+    getLetter();
+  } else {
+    $(this)
+      .css('background-color', '')
+      .css('background-color', 'rgb(122, 77, 158)')
+      .css('color','')
+      .css('color','rgb(255, 255, 255)');
+    used_letters.push(chosen_letter);
+    console.log('letters used ' + used_letters);
   }
-
-  function getLetter() {
-    $('.Grid-cell').off().on('click', function(e) {
-      var chosen_letter = $(this).text();
-      console.log('you choose: ' + chosen_letter)
-    if(used_letters.indexOf(chosen_letter) > -1) {
-      // alert('you already played that letter');
-      getLetter();
-    } else {
-      $(this)
-        .css('background-color', '')
-        .css('background-color', 'rgb(122, 77, 158)')
-        .css('color','')
-        .css('color','rgb(255, 255, 255)');
-      used_letters.push(chosen_letter);
-      console.log('letters used ' + used_letters);
-    }
 
     function isLetterInWord(chosen_letter) {
       if(word_array.indexOf(chosen_letter) > -1) {
@@ -82,7 +127,7 @@ $(document).ready(function(){
         // console.log('you found the word.  the word is ' + (word_array.join('')));
         won += 1;
         console.log('won: ' + won + ' - lost: ' + lost);
-        $('.image').append('Correct, the word was: '+ '<br>' + word);
+        // $('.image').append('Correct, the word was: '+ '<br>' + word);
         $('.word_to_guess').show();
         // getWord();
         playAgain();
@@ -92,7 +137,7 @@ $(document).ready(function(){
       }
     } else {
       lost +=1;
-      $('.image').append('you loose the word you tried to find is: ' + word);
+      // $('.image').append('you loose the word you tried to find is: ' + word);
       console.log('you loose');
       playAgain();
       endGame();
@@ -100,29 +145,25 @@ $(document).ready(function(){
   }
 
   function playAgain() {
-    // var continue_game = prompt('do you want to keep on playing?')
-    // if(continue_game === 'Y') {
-    // setTimeout(function() {
-    // $('.word_to_guess').empty();
-    // }, 1500);
-    var error_count = 0;
-    var used_letters = [];
-    var index_chosen_letter= [];
+    error_count = 0;
+    used_letters = [];
+    index_chosen_letter= [];
     $('.word_to_guess').val('').focus();
+    $('.word_to_guess').hide();
+
+    // $('.lines').empty();
     $('.Grid-cell')
+      .css('transform', 'scale(' + 1 + ')')
       .css('background-color', '')
       .css('background-color', 'rgb(50, 57, 61)')
       .css('color','')
       .css('color', 'rgb(96, 96, 91)');
-    getWord();
-    // } else {
-    //   endGame();
-    // }
+    aiOrHuman();
   }
 
 
   function endGame() {
     console.log('game ended');
-    // thisFunctionDoesNotExistAndWasCreatedWithTheOnlyPurposeOfStopJavascriptExecutionOfAllTypesIncludingCatchAndAnyArbitraryWeirdScenario();
   }
-})
+
+// })
